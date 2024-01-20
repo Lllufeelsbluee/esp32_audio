a3875f3900fae32aca6091066774f59e85736c75
Lllufeelsbluee <21161353@student.hcmute.edu.vn>
2024-01-20 22:46:32 +0700

    second commit

--- i2s_sampling/src/main.cpp
+++ i2s_sampling/src/main.cpp
@@ -1,20 +1,13 @@
 #include <Arduino.h>
-#include <WiFi.h>
-#include <HTTPClient.h>
-#include "WiFiCredentials.h"
+// #include <WiFi.h>
+// #include <HTTPClient.h>
+// #include "WiFiCredentials.h"
 #include "I2SMEMSSampler.h"
 #include "ADCSampler.h"
 
-WiFiClient *wifiClientADC = NULL;
-HTTPClient *httpClientADC = NULL;
-WiFiClient *wifiClientI2S = NULL;
-HTTPClient *httpClientI2S = NULL;
-ADCSampler *adcSampler = NULL;
-I2SSampler *i2sSampler = NULL;
+ ADCSampler *adcSampler = NULL;
+ I2SSampler *i2sSampler = NULL;
 
-// replace this with your machines IP Address
-#define ADC_SERVER_URL "http://192.168.1.72:5003/adc_samples"
-#define I2S_SERVER_URL "http://192.168.1.72:5003/i2s_samples"
 
 // i2s config for using the internal ADC
 i2s_config_t adcI2SConfig = {
@@ -54,85 +47,63 @@ i2s_pin_config_t i2sPins = {
 // how many samples to read at once
 const int SAMPLE_SIZE = 16384;
 
-// send data to a remote address
-void sendData(WiFiClient *wifiClient, HTTPClient *httpClient, const char *url, uint8_t *bytes, size_t count)
-{
-  // send them off to the server
-  digitalWrite(2, HIGH);
-  httpClient->begin(*wifiClient, url);
-  httpClient->addHeader("content-type", "application/octet-stream");
-  httpClient->POST(bytes, count);
-  httpClient->end();
-  digitalWrite(2, LOW);
-}
 
 // Task to write samples from ADC to our server
-void adcWriterTask(void *param)
-{
+void adcWriterTask(void *param) {
   I2SSampler *sampler = (I2SSampler *)param;
   int16_t *samples = (int16_t *)malloc(sizeof(uint16_t) * SAMPLE_SIZE);
-  if (!samples)
-  {
+  if (!samples) {
     Serial.println("Failed to allocate memory for samples");
     return;
   }
-  while (true)
-  {
+  while (true) {
     int samples_read = sampler->read(samples, SAMPLE_SIZE);
-    sendData(wifiClientADC, httpClientADC, ADC_SERVER_URL, (uint8_t *)samples, samples_read * sizeof(uint16_t));
-  }
+    
+    for (int i = 0; i < samples_read; i++)
+    {
+     printf("adc: %d \n",samples[i]);
+     
+    }
+  
+   
+    
+  }  
 }
 
-// Task to write samples to our server
-void i2sMemsWriterTask(void *param)
-{
+void i2sMemsWriterTask(void *param) {
   I2SSampler *sampler = (I2SSampler *)param;
   int16_t *samples = (int16_t *)malloc(sizeof(uint16_t) * SAMPLE_SIZE);
-  if (!samples)
-  {
+  if (!samples) {
     Serial.println("Failed to allocate memory for samples");
     return;
   }
-  while (true)
-  {
+  while (true) {
     int samples_read = sampler->read(samples, SAMPLE_SIZE);
-    sendData(wifiClientI2S, httpClientI2S, I2S_SERVER_URL, (uint8_t *)samples, samples_read * sizeof(uint16_t));
+    
+    for (int i = 0; i < samples_read; i++)
+    {
+      printf("i2s:%d\n ",samples[i]);
+      
+    }
+    
+   
   }
 }
 
 void setup()
 {
   Serial.begin(115200);
-  // launch WiFi
-  Serial.printf("Connecting to WiFi");
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(SSID, PASSWORD);
-  while (WiFi.waitForConnectResult() != WL_CONNECTED)
-  {
-    Serial.print(".");
-    delay(1000);
-  }
-  Serial.println("");
-  Serial.println("WiFi Connected");
-  Serial.println("Started up");
-  // indicator LED
-  pinMode(2, OUTPUT);
-  // setup the HTTP Client
-  wifiClientADC = new WiFiClient();
-  httpClientADC = new HTTPClient();
-
-  wifiClientI2S = new WiFiClient();
-  httpClientI2S = new HTTPClient();
+  
 
   // input from analog microphones such as the MAX9814 or MAX4466
   // internal analog to digital converter sampling using i2s
   // create our samplers
-  // adcSampler = new ADCSampler(ADC_UNIT_1, ADC1_CHANNEL_7, adcI2SConfig);
+  adcSampler = new ADCSampler(ADC_UNIT_1, ADC1_CHANNEL_7, adcI2SConfig);
 
-  // set up the adc sample writer task
-  // TaskHandle_t adcWriterTaskHandle;
-  // adcSampler->start();
-  // xTaskCreatePinnedToCore(adcWriterTask, "ADC Writer Task", 4096, adcSampler, 1, &adcWriterTaskHandle, 1);
+  //set up the adc sample writer task
+  TaskHandle_t adcWriterTaskHandle;
+  adcSampler->start();
+  xTaskCreatePinnedToCore(adcWriterTask, "ADC Writer Task", 4096, adcSampler, 1, &adcWriterTaskHandle, 1);
 
   // Direct i2s input from INMP441 or the SPH0645
   i2sSampler = new I2SMEMSSampler(I2S_NUM_0, i2sPins, i2sMemsConfigLeftChannel, false);
@@ -144,7 +115,6 @@ void setup()
   // // start sampling from i2s device
 }
 
-void loop()
-{
-  // nothing to do here - everything is taken care of by tasks
+void loop() {
+ 
 }
